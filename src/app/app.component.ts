a193157acebcafeb9587042cbb5beeccf6d8f9cd
AshishSangar <ashishsangar2398@gmail.com>
2024-06-07 15:05:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todoapp';
+  title = 'angular-todo-crud';
+
+  list:any[]=[];
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item})
+    console.log(this.list)
+  }
+
+  removeTask(id:number){
+    this.list=this.list.filter(item=>item.id!==id)
+  }
 }
