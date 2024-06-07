73b80e5fc17178ea9436500111fb785a04557506
Ashish Sangar <89684664+AshishSangar@users.noreply.github.com>
2024-06-07 15:13:02 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,1 @@
-export const environment = {
-    production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyA0dZ4MvetXn0UQlsKxwjgGecxCAnr1u_w",
-        authDomain: "todo-48e7a.firebaseapp.com",
-        projectId: "todo-48e7a",
-        storageBucket: "todo-48e7a.appspot.com",
-        messagingSenderId: "1096787785900",
-        appId: "1:1096787785900:web:d2c4d195f8e374381f4709",
-    }
-  };
+
