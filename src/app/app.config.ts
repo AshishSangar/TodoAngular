6645e1c29e45ef8ddd4ce6a86cb15c3f1c89de95
Ashish Sangar <89684664+AshishSangar@users.noreply.github.com>
2024-06-07 15:13:35 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
